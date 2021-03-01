6f7bcd7170d300d13129b5736f0ff6f0ea39c0e1
Eli Markov <eli.markov@outlook.com>
2021-03-01 17:05:18 +0000

    Add rows and columns to Matrix

--- src/Maths/Matrix.py
+++ src/Maths/Matrix.py
@@ -1,3 +1,13 @@
 class Matrix():
-  def __init__(self) -> None:
-      pass
+  def __init__(self, rows: int, columns:  int) -> None:
+      self.__rows = rows
+      self.__columns = columns
+
+  @property
+  def get_rows(self):
+    return self.__rows
+
+  @property
+  def get_columns(self):
+    return self.__columns
+
--- tests/testMatrix.py
+++ tests/testMatrix.py
@@ -5,7 +5,7 @@ from src.Maths.Matrix import Matrix
 class TestMatrix(unittest.TestCase):
 
   def test_ctor(self):
-    self.assertIsNotNone(Matrix())
+    self.assertIsNotNone(Matrix(10, 20))
 
 if __name__ == '__main__':
     unittest.main()
